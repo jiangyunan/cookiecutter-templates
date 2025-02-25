155f3b8e294dc80a93d91bd730c87279db56d791
云安 蒋 <jiangyunan@carnoc.com>
2025-02-25 10:59:43 +0800

    Initial commit

---
+++ fastapi-template/{{cookiecutter.project_name}}/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from .routers import items
+
+app = FastAPI()
+
+app.include_router(items.router)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
