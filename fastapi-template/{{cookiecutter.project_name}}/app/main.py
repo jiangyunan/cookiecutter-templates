0e8289462fe481c8974f4e243e2f17dc4a214b9c
云安 蒋 <jiangyunan@carnoc.com>
2025-02-25 11:34:40 +0800

    u

--- fastapi-template/{{cookiecutter.project_name}}/app/main.py
+++ fastapi-template/{{cookiecutter.project_name}}/app/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .routers import items
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+# Mount the static files directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Set up templates
+templates = Jinja2Templates(directory="templates")
 
 app.include_router(items.router)
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
